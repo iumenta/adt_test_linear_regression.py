2ee3fd46c72abe51d3e0c21320892743f10caae7
Kristina Vodorezova <164237440+vodor001@users.noreply.github.com>
2024-08-14 16:34:17 +0200

    Update app.sh

--- app/app.sh
+++ app/app.sh
@@ -10,9 +10,9 @@
 # Clone the repository of your tool and checkout to one specific commit. 
 #########################################################
 
-# git clone https://github.com/odtp-org/tool-example.git /odtp/odtp-workdir/tool-example
-# cd /odtp/odtp-workdir/tool-example
-# git checkout xxxxxxxxxxxx
+git clone https://github.com/vodor001/test_linear_regression.py.git /odtp/odtp-workdir/test_linear_regression
+cd /odtp/odtp-workdir/test_linear_regression
+git checkout v0.1.0
 
 #########################################################
 # 2. CONFIG FILE CONFIGURATION
@@ -25,7 +25,7 @@
 # 3. INPUT FOLDER MANAGEMENT
 #########################################################
 
-# ln -s /odtp/odtp-input/... /odtp/odtp-workdir/...
+ln -s /odtp/odtp-input/test_linear_regression/data /odtp/odtp-workdir/test_linear_regression/data
 
 #########################################################
 # 4. TOOL EXECUTION
@@ -33,13 +33,13 @@
 #########################################################
 
 # COMMAND $PARAMETER_A #PARAMETER_B /odtp/odtp-input/data
-
+python linear_regression.py /../../odtp-input/linear_regression/data
 #########################################################
 # 5. OUTPUT FOLDER MANAGEMENT
 # The selected output files generated should be placed in the output folder
 #########################################################
 
-# cp -r /odtp/odtp-workdir/output/* /odtp/odtp-output
+cp -r /odtp/odtp-workdir/output/* /odtp/odtp-output/test_linear_regression/data
 
 ############################################################################################
 # END OF MANUAL USER APP
